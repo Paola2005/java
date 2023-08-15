7f45c628a0bd2eda3ae7d4ee46afd60dea5aa323
Paola2005 <velascopaola2005@gmail.com>
2023-08-14 20:29:27 -0500

    JavaScript

--- trabajo.js
+++ trabajo.js
@@ -1,13 +1,27 @@
-function arreglo(numero,suma){
-    let parejas=[]
-    for (i=0;i<numero.length;i++){
-        for (j=1;j<numero.length;j++){
-            if(numero[i]+numero[j]==suma){
-                parejas.push([numero[i],numero[j]])
-        
+//########################################
+//Trabajo en clase
+//########################################
+
+//Escribe una función que tome un array de números y un valor objetivo
+//como entrada, y devuelva un array de arrays que contengan pares de
+//números en el array original que sumen al valor objetivo.
+
+
+function arreglo(numero, suma) {
+    let parejas = []; //aca se almacenan las parejas que suman el valor de 10
+    //el length es para obtener la longitud o cantidad de elementos
+    for (i = 0; i < numero.length; i++) { // Itera a través de los elementos de la funcion numero.
+        for (j = 1; j < numero.length; j++) { // Itera a través de los elementos de la funcion numero a partir de la posición siguiente a i
+            if (numero[i] + numero[j] == suma) { // Verifica si la suma de los elementos es igual al valor suma
+                parejas.push([numero[i], numero[j]]); // Inserta la pareja de elementos a la funcion parejas 
             }
         }
     }
-    return parejas
+    
+    return parejas; // Devuelve el arreglo que contiene las parejas que cumplen con la condición
 }
-console.log(arreglo([2,4,3,6,9,8],10));
+
+console.log(arreglo([2, 4, 3, 6, 9, 8], 10)); // Llama a la función con el arreglo [2, 4, 3, 6, 9, 8] y el valor 10 como argumentos, luego imprime el resultado.
+
+
+//Desarrolado por: Jenny Paola Velasco Manrique
---
+++ trabajo2.js
@@ -0,0 +1,16 @@
+//################################################################
+//Trabajo que hice en la tarde porque estaba desparchada
+//################################################################
+
+//Se pide que se haga una lista de numeros del 0 al 10 y que se informe cuales numero son pares o impares
+
+for (let i = 0; i <=10; i++){
+    if (i%2==0){
+        console.log("El numero",i, "Es par");
+    }
+    else{
+        console.log("El numero",i, "Es impar");
+    }
+}
+
+//Desarrolado por: Jenny Paola Velasco Manrique
